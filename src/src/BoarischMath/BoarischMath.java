ffe45b05093cb36e93ecc5825ecbf1ef50c34205
Jan Aiko Niederau <janaiko.niederau@outlook.de>
2024-01-17 21:01:13 +0100

    [ADD] BoarischMath.java

---
+++ src/src/BoarischMath/BoarischMath.java
@@ -0,0 +1,11 @@
+package BoarischMath;
+
+public class BoarischMath {
+    public static double boarischAbs(double a) {
+        return Math.abs(a);
+    }
+
+    public static float boarischAbs(float a) {
+        return Math.abs(a);
+    }
+}
